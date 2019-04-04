fb3a3b18d37b8bb7c2d53008379559ba1dd377d0
Cristian Apostu <cristy_apostu@yahoo.com>
2019-04-04 21:31:38 +0300

    studied custom exceptions; continued CRUD service; updated configs

--- src/test/java/org/fasttrackit/onlineshopapi/ProductServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/onlineshopapi/ProductServiceIntegrationTests.java
@@ -1,8 +1,10 @@
 package org.fasttrackit.onlineshopapi;
 
 import org.fasttrackit.onlineshopapi.domain.Product;
+import org.fasttrackit.onlineshopapi.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshopapi.service.ProductService;
 import org.fasttrackit.onlineshopapi.transfer.CreateProductRequest;
+import org.fasttrackit.onlineshopapi.transfer.UpdateProductRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.IsNull.notNullValue;
 
 @RunWith(SpringRunner.class)
@@ -19,18 +21,72 @@ public class ProductServiceIntegrationTests {
 
     @Autowired
     private ProductService productService;
-    @Test
 
+    @Test
     public void testCreateProduct_whenValidRequest_thenReturnProductWithId(){
+        Product product = createProduct();
+
+        assertThat(product, notNullValue());
+        assertThat(product.getId(), greaterThan(0L));
+    }
+
+    private Product createProduct() {
         CreateProductRequest request = new CreateProductRequest();
         request.setName("Laptop");
         request.setPrice(10);
         request.setQuantity(3);
         request.setSku("fdas321k231");
 
-        Product product = productService.createProduct(request);
+        return productService.createProduct(request);
+    }
 
-        assertThat(product, notNullValue());
-        assertThat(product.getId(), greaterThan(0L));
+    @Test(expected = ResourceNotFoundException.class)
+    public void testGetProduct_whenProductNotFound_thenThrowResourceNotFoundException() throws ResourceNotFoundException {
+        productService.getProduct(0);
+    }
+
+    @Test
+    public void testGetProduct_whenExistingId_thenReturnMatchingProduct() throws ResourceNotFoundException {
+       Product product = createProduct();
+
+        Product retrievedProduct = productService.getProduct(product.getId());
+
+        assertThat(retrievedProduct.getId(), is(product.getId()));
+        assertThat(retrievedProduct.getName(), is(product.getName()));
+
+    }
+
+    @Test
+    public void testUpdateProduct_whenValidRequestWithAllFields_thenReturnUpdatedProduct() throws ResourceNotFoundException {
+        Product createdProduct = createProduct();
+
+        UpdateProductRequest request = new UpdateProductRequest();
+        request.setName(createdProduct.getName() + "Edited");
+        request.setPrice(createdProduct.getPrice() + 10);
+        request.setQuantity(createdProduct.getQuantity() + 10);
+        request.setSku(createdProduct.getSku() + "Edited");
+
+        Product updatedProduct = productService.updateProduct(createdProduct.getId(), request);
+
+        assertThat(updatedProduct.getName(), is(request.getName()));
+        assertThat(updatedProduct.getName(), not(createdProduct.getName()));
+
+        assertThat(updatedProduct.getPrice(), is(request.getPrice()));
+        assertThat(updatedProduct.getQuantity(), is(request.getQuantity()));
+        assertThat(updatedProduct.getSku(), is(request.getSku()));
+
+        assertThat(updatedProduct.getId(), is(createdProduct.getId()));
+    }
+
+//    todo: Implement negative tests for update and tests for update with some fields only
+
+    @Test
+    public void testDeleteProduct_whenExistingId_thenProductIsDeleted() throws ResourceNotFoundException {
+        Product createdProduct = createProduct();
+
+        productService.deleteProduct(createdProduct.getId());
+
+        productService.getProduct(createdProduct.getId());
+
     }
 }
